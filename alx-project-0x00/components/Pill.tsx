86876884335457b04247f59d122fa8a007b314eb
oGhartey <ondghartey@gmail.com>
2025-11-16 14:50:15 +0000

    Rendering components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Pill = () => {
-  return <div>Pill</div>;
+const Pill: React.FC = () => {
+  return (
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">Title</p>
+    </div>
+  );
 };
 
 export default Pill;
